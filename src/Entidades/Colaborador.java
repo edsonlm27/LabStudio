a2f332e5b3a4b7138bb514faa8f6a2d1ee9fc596
edsonlm27 <edsonlm27@gmail.com>
2023-05-21 20:26:36 -0300

    Projeto para organizar um sistema de colaboradores seguindo o conceito de cargos para Devs

---
+++ src/Entidades/Colaborador.java
@@ -0,0 +1,69 @@
+package Entidades;
+
+import java.util.Date;
+
+public class Colaborador {
+    private String nome;
+    private Date dtAdmissao;
+    private Date dtDesligamento;
+    private Cargo cargo;
+    private double salario;
+
+    public Colaborador(String nome, Date date, Cargo cargo) {
+        this.nome = nome;
+        this.dtAdmissao = dtAdmissao;
+        this.cargo = this.cargo;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDtAdmissao() {
+        return dtAdmissao;
+    }
+
+    public void setDtAdmissao(Date dtAdmissao) {
+        this.dtAdmissao = dtAdmissao;
+    }
+
+    public Date getDtDesligamento() {
+        return dtDesligamento;
+    }
+
+    public void setDtDesligamento(Date dtDesligamento) {
+        this.dtDesligamento = dtDesligamento;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Colaborador{" +
+                "nome='" + nome + '\'' +
+                ", dtAdmissao=" + dtAdmissao +
+                ", dtDesligamento=" + dtDesligamento +
+                ", cargo=" + cargo +
+                ", salario=" + salario +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import Entidades.Cargo;
+import Entidades.Colaborador;
+import Entidades.Nivel;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main {
+
+    /*Ex 4 - Fazer inclusão de novo colaborador em lista de Colaboradores
+    Faça um método para inclusão de um novo colaborador na lista de colaboradores
+*/
+
+    public static void main(String[] args) {
+
+        ArrayList<Cargo> cargos = new ArrayList<>();
+        ArrayList<Colaborador> colaboradores = new ArrayList<>();
+
+        cargos.add(new Cargo("Desenvolvedor Front-end Junior", 2500.0, Nivel.JUNIOR));
+        cargos.add(new Cargo("Desenvolvedor Front-end Pleno", 4500.0, Nivel.PLENO));
+        cargos.add(new Cargo("Desenvolvedor Front-end Senior", 6500.0, Nivel.SENIOR));
+
+        colaboradores.add(new Colaborador("Alberto", new Date(), cargos.get(0)));
+        colaboradores.add(new Colaborador("Eduardo", new Date(), cargos.get(1)));
+        colaboradores.add(new Colaborador("Gerson", new Date(), cargos.get(2)));
+
+    }
+}
